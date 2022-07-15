3547765b0b1830724de7710e0aa8690d7d531379
RaduTuodr <tudoreduard2004@gmail.com>
2022-07-15 19:31:30 +0300

    Start Activities

---
+++ app/src/main/java/com/example/lucky13/activities/start_activities/CreateDoctorAccount.java
@@ -0,0 +1,16 @@
+package com.example.lucky13.activities.start_activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lucky13.R;
+
+public class CreateDoctorAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_doctor_account);
+    }
+}
---
+++ app/src/main/java/com/example/lucky13/activities/start_activities/CreatePatientAccount.java
@@ -0,0 +1,18 @@
+package com.example.lucky13.activities.start_activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lucky13.R;
+
+public class CreatePatientAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_patient_account);
+
+
+    }
+}
