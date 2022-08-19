111f3556145647d7f1d15401f10c8c211e171cd5
Tudor Pop <104305749+tudorp06@users.noreply.github.com>
2022-08-19 16:27:18 +0300

    Add files via upload

---
+++ app/src/main/java/com/example/lucky13/activities/BMICalculatorActivity.java
@@ -0,0 +1,87 @@
+package com.example.lucky13.activities.patient_path;
+
+import static com.example.lucky13.utils.Utils.calculateBMIValue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.lucky13.R;
+import com.example.lucky13.activities.common_activities.WelcomePage;
+import com.kevalpatel2106.rulerpicker.RulerValuePicker;
+import com.kevalpatel2106.rulerpicker.RulerValuePickerListener;
+
+import java.text.DecimalFormat;
+
+public class BMICalculatorActivity extends AppCompatActivity {
+
+
+    RulerValuePicker weightRulerPicker,
+                    heightRulerPicker;
+    Button correctBMIButton;
+    TextView bmiText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmicalculator);
+
+        weightRulerPicker = findViewById(R.id.bmi_calculator_weightPicker);
+        heightRulerPicker = findViewById(R.id.bmi_calculator_heightPicker);
+        bmiText = findViewById(R.id.bmi_calculator_bmiTextView);
+        correctBMIButton = findViewById(R.id.bmi_calculator_correctBMIButton);
+
+        DecimalFormat df =new DecimalFormat("#.#");
+
+        weightRulerPicker.selectValue(60);
+        heightRulerPicker.selectValue(150);
+
+        final double[] weight = new double[1];
+        final double[] height = new double[1];
+
+        weightRulerPicker.setValuePickerListener(new RulerValuePickerListener() {
+            @Override
+            public void onValueChange(final int selectedValue) {
+
+                weight[0] = weightRulerPicker.getCurrentValue();
+
+                bmiText.setText(df.format(calculateBMIValue(weight[0], height[0])));
+            }
+
+            @Override
+            public void onIntermediateValueChange(final int selectedIntermediateValue) {
+
+            }
+        });
+
+        heightRulerPicker.setValuePickerListener(new RulerValuePickerListener() {
+            @Override
+            public void onValueChange(int i) {
+
+                height[0] = (double) heightRulerPicker.getCurrentValue() / 100;
+
+                bmiText.setText(df.format(calculateBMIValue(weight[0], height[0])));
+            }
+
+            @Override
+            public void onIntermediateValueChange(int i) {
+
+            }
+        });
+
+        correctBMIButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //TODO: trebuie sa mai modificam sa integram in path-ul pacientului (adica si extra urile)
+                Intent intent = new Intent(BMICalculatorActivity.this, WelcomePage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
