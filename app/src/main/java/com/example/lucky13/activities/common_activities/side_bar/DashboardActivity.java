7320f2baa5046b62d3e252bf1e6a43e1d6c4ed77
RaduTuodr <tudoreduard2004@gmail.com>
2022-08-23 01:39:23 +0300

    sidebar impl - only layouts

---
+++ app/src/main/java/com/example/lucky13/activities/common_activities/side_bar/DashboardActivity.java
@@ -0,0 +1,18 @@
+package com.example.lucky13.activities.common_activities.side_bar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lucky13.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+    }
+}
