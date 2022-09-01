78c3051e8ae1ad0d2f93907a222e2279629e7652
RaduTuodr <tudoreduard2004@gmail.com>
2022-09-01 20:33:16 +0300

    YourProfile page implementation

--- app/src/main/java/com/example/lucky13/activities/patient_path/menu_options/YourProfileActivity.java
+++ app/src/main/java/com/example/lucky13/activities/patient_path/menu_options/YourProfileActivity.java
@@ -1,20 +1,89 @@
 package com.example.lucky13.activities.patient_path.menu_options;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.lucky13.R;
 import com.example.lucky13.activities.patient_path.side_bar.DrawerBaseActivity;
-import com.example.lucky13.databinding.ActivityDashboardBinding;
+import com.example.lucky13.models.Patient;
+import com.example.lucky13.service.PatientService;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.ArrayList;
 
 public class YourProfileActivity extends DrawerBaseActivity {
 
-    ActivityDashboardBinding activityDashboardBinding;
+    TextView name,
+            email,
+            birthday,
+            gender;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    PatientService patientService = new PatientService();
+    ArrayList<Patient> allPatients = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_your_profile);
 
-        activityDashboardBinding = ActivityDashboardBinding.inflate(getLayoutInflater());
-        setContentView(activityDashboardBinding.getRoot());
-        allocateActivityTitle("Your Profile");
+        initWidgets();
+
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser != null) {
+
+            patientService.getAllPatients();
+            patientService.patientList.observe(this, patients -> {
+
+                allPatients.addAll(patients);
+
+                displayUserData(firebaseUser);
+            });
+        }
+    }
+
+    private void initWidgets() {
+
+        name = (TextView) findViewById(R.id.yourProfileFullNameText);
+        email = (TextView) findViewById(R.id.yourProfileEmailText);
+        birthday = (TextView) findViewById(R.id.yourProfileBirthdayText);
+        gender = (TextView) findViewById(R.id.yourProfileGenderText);
+    }
+
+    @Nullable
+    private Patient getPatientByUID(String UID) {
+
+        for (Patient patient: allPatients) {
+
+            if (patient.getUID().equals(UID))
+                return patient;
+        }
+
+        return null;
+    }
+
+    private void displayUserData(@NonNull FirebaseUser firebaseUser) {
+
+        String UID = firebaseUser.getUid();
+        Patient patient = getPatientByUID(UID);
+
+        assert patient != null;
+        String fullName = patient.getFirstName() + " " + patient.getLastName();
+        String mail = patient.getEmail();
+        String birthdayF = patient.getDateOfBirth().getFirst().toString() + "/" +
+                            patient.getDateOfBirth().getSecond().toString() + "/" +
+                            patient.getDateOfBirth().getThird().toString();
+        String sex = patient.getGender().equals("male")
+                        ? "Male"
+                        : "Female";
+
+        name.setText(fullName);
+        email.setText(mail);
+        birthday.setText(birthdayF);
+        gender.setText(sex);
     }
 }
